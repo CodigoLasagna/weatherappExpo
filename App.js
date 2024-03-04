2a7e3b2580040a5b89fbacbdf431f65ca845bce5
Hanck Eddison <hanckeddison@gmail.com>
2024-03-04 15:42:15 -0800

    add_data

--- App.js
+++ App.js
@@ -3,16 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import MainView from './src/modules/MainView';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-	  <MainView/>
-    </View>
-  );
+	return (
+		<MainView/>
+	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#1B232B',
-	},
-});
